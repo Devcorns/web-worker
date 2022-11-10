917491e3aa8ba9b11ecbc6a9ef2b4916c3e5c0d8
Devcorns Innovation <itprakhar@gmail.com>
2022-11-10 23:29:22 +0530

    Put comment

--- main-thread.js
+++ main-thread.js
@@ -1,13 +1,18 @@
 const worker1 = new Worker('worker.js?v=6');
-
-
-const btn1 = document.getElementById("btn1")
-const btn2 = document.getElementById("btn2")
-var isTrue = false
-// 
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+var isTrue = false;
+ 
+/**
+ * un comment below code to see how for loop stcuk main thread
+ */
 btn1.addEventListener("click",function(e) {
+    // let sum = 0;
+    //     for(let i = 0;i<10000000000;i++) {
+    //         sum += i;
+    //     }
+    //     console.log(sum)
     worker1.postMessage("hello")
-    console.log(isTrue,isTrue)
 })
 
 btn2.addEventListener("click",function(e) {
